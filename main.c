7d31e9828549f0866dadc73d72c99f919dcf779f
Alessandro Diaferia <alediaferia@gmail.com>
2025-03-31 12:33:25 +0200

    chore: refactoring

--- main.c
+++ main.c
@@ -53,21 +53,52 @@ void* read_file_content(const char *filepath, size_t *content_length) {
         return NULL;
     }
 
-    // The file descriptor can be closed after mmap succeeds
+    /* The file descriptor can be closed after mmap succeeds */
     close(fd);
 
     *content_length = st.st_size;
     return mapped;
 }
 
+size_t calc_stdio_buffer_size() {
+    struct stat s;
+
+    int fd = fcntl(STDIN_FILENO, F_DUPFD, 0);
+    fstat(fd, &s);
+    close(fd);
+
+    size_t bufsize = s.st_blksize;
+    if (bufsize <= 0) {
+        /* try to use a page size from the system */
+        bufsize = sysconf(_SC_PAGESIZE);
+        if (bufsize <= 0) {
+            bufsize = FALLBACK_BUFFER_SIZE; // arbitrary fall back
+        }
+    }
+
+    return bufsize;
+}
+
+#define DEFAULT_SEPARATOR_VALUE '\n'
+
+typedef struct map_config {
+    char *map_value; // the value to map to
+    size_t map_value_length;
+    char separator;
+    char concatenator;
+} map_config_t;
+
+map_config_t new_map_config() {
+    return (map_config_t){NULL, 0, DEFAULT_SEPARATOR_VALUE, 0};
+}
+
 int main(int argc, char *argv[]) {
     int opt;
-    char *s_value_arg = NULL;
     char *value_file_path = NULL;
-    char s_concat_arg = 0;
-    char s_sep_arg = '\n';
-    int value_from_file = 0;
-    size_t s_value_arg_len = 0;
+
+    map_config_t map_config = new_map_config();
+
+    int value_from_file_flag = 0;
 
     /* Define long options */
     static struct option long_options[] = {
@@ -78,27 +109,27 @@ int main(int argc, char *argv[]) {
     while ((opt = getopt_long(argc, argv, "s:c:v:", long_options, NULL)) != -1) {
         switch (opt) {
             case 'v':
-                if (value_from_file) {
+                if (value_from_file_flag) {
                     fprintf(stderr, "Error: Cannot specify both -v and --value-file\n");
                     print_usage(argv);
                     exit(EXIT_FAILURE);
                 }
-                s_value_arg = optarg;
+                map_config.map_value = optarg;
                 break;
             case 'f': /* --value-file option */
-                if (s_value_arg != NULL) {
+                if (map_config.map_value != NULL) {
                     fprintf(stderr, "Error: Cannot specify both -v and --value-file\n");
                     print_usage(argv);
                     exit(EXIT_FAILURE);
                 }
                 value_file_path = optarg;
-                value_from_file = 1;
+                value_from_file_flag = 1;
                 break;
             case 's':
-                parse_single_char_arg(optarg, &s_sep_arg, opt, argv);
+                parse_single_char_arg(optarg, &(map_config.separator), opt, argv);
                 break;
             case 'c':
-                parse_single_char_arg(optarg, &s_concat_arg, opt, argv);
+                parse_single_char_arg(optarg, &(map_config.concatenator), opt, argv);
                 break;
             case '?':
             default:
@@ -108,38 +139,27 @@ int main(int argc, char *argv[]) {
     }
 
     /* Handle value from file if specified */
-    if (value_from_file) {
-        s_value_arg = read_file_content(value_file_path, &s_value_arg_len);
-        if (s_value_arg == NULL) {
+    if (value_from_file_flag) {
+        map_config.map_value = read_file_content(value_file_path, &(map_config.map_value_length));
+        if (map_config.map_value == NULL) {
             /* Error message already printed in read_file_content */
             exit(EXIT_FAILURE);
         }
-    } else if (s_value_arg == NULL) {
+    } else if (map_config.map_value == NULL) {
         /* Neither -v nor --value-file specified */
         fprintf(stderr, "Error: Either -v or --value-file must be specified\n");
         print_usage(argv);
         exit(EXIT_FAILURE);
     } else {
-        s_value_arg_len = strlen(s_value_arg);
+        map_config.map_value_length = strlen(map_config.map_value);
     }
 
-    if (s_concat_arg == 0) {
-        s_concat_arg = s_sep_arg;
+    /* defaulting the concatenation argument to the separator one if unspecified */
+    if (map_config.concatenator == 0) {
+        map_config.separator = map_config.concatenator;
     }
         
-    /* calculate the optimal buffer size for stdout/stdin */ 
-    struct stat s;
-    int fd = fcntl(STDIN_FILENO, F_DUPFD, 0);
-    fstat(fd, &s);
-    close(fd);
-    size_t bufsize = s.st_blksize;
-    if (bufsize <= 0) {
-        /* try to use a page size from the system */
-        bufsize = sysconf(_SC_PAGESIZE);
-        if (bufsize <= 0) {
-            bufsize = FALLBACK_BUFFER_SIZE; // arbitrary fall back
-        }
-    }
+    size_t bufsize = calc_stdio_buffer_size();
 
     char *buffer = calloc(bufsize, sizeof(char));
     if (buffer == NULL) {
@@ -166,8 +186,8 @@ int main(int argc, char *argv[]) {
     while ((bytes_read = fread(buffer, sizeof(char), bufsize, stdin)) > 0) {
         /* scan the input for the separator char */
         for (size_t i = 0; i < bytes_read; i++) {
-            if (buffer[i] == s_sep_arg) {
-                size_t remaining = s_value_arg_len;
+            if (buffer[i] == map_config.separator) {
+                size_t remaining = map_config.map_value_length;
                 while (remaining > 0) {
                     size_t len = (remaining < bufsize - obuffer_pos) ? remaining : bufsize - obuffer_pos;
                     if (len == 0) {
@@ -180,7 +200,7 @@ int main(int argc, char *argv[]) {
                         }
                         obuffer_pos = 0;
                     } else {
-                        memcpy(obuffer + obuffer_pos, s_value_arg, len);
+                        memcpy(obuffer + obuffer_pos, map_config.map_value, len);
                         obuffer_pos += len;
                         remaining -= len;
                     }
@@ -197,7 +217,7 @@ int main(int argc, char *argv[]) {
                     }
                     obuffer_pos = 0;
                 }
-                obuffer[obuffer_pos++] = s_concat_arg;
+                obuffer[obuffer_pos++] = map_config.concatenator;
             }
         }
 
